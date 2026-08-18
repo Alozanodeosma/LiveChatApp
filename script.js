6eb19a534a0c37125e864730975e26f18109520f
Alvaro Lozano de Osma <74834169+Alozanodeosma@users.noreply.github.com>
2026-08-18 19:02:02 +0200

    Custom Username

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
+
 const messages = document.querySelector(".messages");
 const inputText = document.querySelector(".text-input");
 inputText.addEventListener("keypress", sendText);
-let username = document.querySelector(".user-name").textContent;
+let username = document.querySelector(".user-name");
+
+
+//Text bar 
+let prefix = ` ${username.value} > `;
+inputText.value = prefix;
+inputText.addEventListener('input', () => {
+  if (!inputText.value.startsWith(prefix)) {
+    inputText.value = prefix;
+  }
+});
 
 function sendText(key){
     if (key.key == "Enter"){
         let newMessage = document.createElement('p');
-        newMessage.textContent=`${username} > ${inputText.value}`;
+        newMessage.textContent=`${inputText.value}`;
         newMessage.classList="message";
-        messages.appendChild(newMessage);
-        inputText.value="";
+        messages.prepend(newMessage);
+        inputText.value=prefix;
     }
 }
+
+//Username
+username.addEventListener("blur", ()=>{
+    prefix=` ${username.value} > `;
+    inputText.value = prefix;
+    console.log("heloo");
+});
