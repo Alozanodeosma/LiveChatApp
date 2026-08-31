21c22a01497430b8c1aa02f0787b33e23f576f62
Alvaro Lozano de Osma <74834169+Alozanodeosma@users.noreply.github.com>
2026-08-31 23:43:52 +0300

    Chatrooms available but in a really rought shape

--- public/script.js
+++ public/script.js
@@ -1,16 +1,115 @@
-
 const messages = document.querySelector(".messages");
-const inputText = document.querySelector(".text-input");
-inputText.addEventListener("keypress", sendTextToServer);
-let username = document.querySelector(".user-name");
 
+
+let username = document.querySelector(".user-name");
+let currentChatroomId = null;
 const socket = new WebSocket('ws://localhost:3001');
 
-//Client
+//Confirms connection with the server with a client side console.log and sends a message to the server
 socket.addEventListener('open', () => {
   console.log('Conectado al servidor WebSocket');
-  socket.send('¡Hola servidor! Soy el cliente.');
+  //sendServerUpdateCurrentStatus();
 });
+socket.addEventListener('close',()=>{
+  //sendServerUpdateCurrentStatus();
+})
+
+////////////////////////////////////////////////////////////////////////////////////
+///CHATROOM CREATION////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+
+//Create chatroom btn functionality
+const addChatroomBtn = document.querySelector(".new-chat-bubble");
+const addChatroomPopup = document.getElementById("chatroomPopup");
+addChatroomBtn.addEventListener("click",()=>{
+  addChatroomPopup.hidden = !addChatroomPopup.hidden;
+})
+
+//Chatroom creation
+const createNewChatroomBtn = document.getElementById("createNewChatroomBtn");
+createNewChatroomBtn.addEventListener("click", ()=>{
+  //Send a petition to the server to create a chatroom
+  socket.send(JSON.stringify({
+    type: "chatroomCreationPetition"
+  }))
+})
+
+const clientChatrooms = new Map();
+const sidebar = document.querySelector("#sidebar");
+
+    function createChatroom(id, userName){
+        clientChatrooms.set(id,{
+            chatroomName: "NEW CHAT",
+            chatroomUsername: "John Doe",
+            usernames: [userName],
+            messages: [],
+            currentMsg: ""
+        });
+        addButtonToSidebar(id);
+    }
+
+    function addButtonToSidebar(id){
+        const newChatroomBtn = document.createElement("button");
+        newChatroomBtn.id = id;
+        newChatroomBtn.textContent = "O";
+        newChatroomBtn.classList = "chat-bubble";
+        newChatroomBtn.addEventListener("click",()=>{
+          loadChatroom(id);
+        })
+        sidebar.prepend(newChatroomBtn);
+    }
+
+    const chatroomIdInput = document.querySelector("#chatroomcode");
+    const joinChatroomBtn = document.querySelector("#joinChatroomBtn");
+    function joinChatroom(){
+      /* 
+        send a join chatroom request message to the server with the USERNAME of the new user
+        
+        assign this function as an event listener to the send button*/
+        socket.send(JSON.stringify({
+          type:"chatroomJoinPetition",
+          data: {
+            chatroomId: chatroomIdInput.value,
+            username: username.value
+          }
+        }))
+        addChatroomPopup.hidden = true;
+    }
+    joinChatroomBtn.addEventListener("click",()=>{joinChatroom()});
+
+
+
+////////////////////////////////////////////////////////////////////////////////////
+/// INPUT TEXT /////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+
+const inputText = document.querySelector(".text-input");
+inputText.addEventListener("keypress", sendTextToServer);
+
+function sendTextToServer(key){
+    if (key.key == "Enter"){
+      //CLIENT SIDE///////
+        let newMessage = document.createElement('p');
+        newMessage.textContent=`${inputText.value}`;
+        newMessage.classList="message";
+        messages.prepend(newMessage);
+        //push it to the local obj
+        clientChatrooms.get(currentChatroomId).messages.push(inputText.value);
+      ////////////////////
+
+      //SERVERSIDE///////
+        //JSON message with the neccessary properties to be identified on server
+        let textMessageJSON = JSON.stringify({
+          type: "textMessage",
+          chatId:currentChatroomId,
+          data: inputText.value
+        })
+
+        socket.send(textMessageJSON);
+      ///////////////////
+        inputText.value=prefix;
+    }
+}
 
 //Text bar 
 let prefix = ` ${username.value} > `;
@@ -21,29 +120,114 @@ inputText.addEventListener('input', () => {
   }
 });
 
-function sendTextToServer(key){
-    if (key.key == "Enter"){
-        let newMessage = document.createElement('p');
-        newMessage.textContent=`${inputText.value}`;
-        newMessage.classList="message";
-        messages.prepend(newMessage);
+////////////////////////////////////////////////////////////////////////////////////
+///CLIENT MESSAGE HANDLER//////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
 
-        socket.send(inputText.value);
+//When the client recieves a message from the server, does some logic in the CLIENT SIDE
+socket.onmessage = (message)=>{
+  const parsedMessage = JSON.parse(message.data);
+  switch(parsedMessage.type){
 
-        inputText.value=prefix;
-
-    }
+    case "textMessage":
+      //add it to the local chatroom item
+      clientChatrooms.get(parsedMessage.chatId).messages.push(parsedMessage.data);
+      //update only that part if the change is in the current displayed chat
+      if(parsedMessage.chatId==currentChatroomId){
+        loadMessages(currentChatroomId);
+      }
+      break;
+    
+      case "chatroomCode":
+      //popup chatroomcode is
+      createChatroom(parsedMessage.data,username.value);
+      addChatroomPopup.hidden = true;
+      loadChatroom(parsedMessage.data);
+      break;
+    
+      //Username Update Handler
+      case "username":
+      //delete the previous username
+      //search the previous name on the local item and replace it, if the username is new add it
+      let usernameFound = false;
+      for(let i = 0; i<clientChatrooms.get(parsedMessage.chatId).usernames.length; i++){
+          if(clientChatrooms.get(parsedMessage.chatId).usernames[i] === parsedMessage.data.previousUsername){
+              clientChatrooms.get(parsedMessage.chatId).usernames[i] = parsedMessage.data.username;
+              usernameFound=true;
+          }
+      }
+      if (!usernameFound) clientChatrooms.get(parsedMessage.chatId).usernames.push(parsedMessage.data.username);
+      //update only that part if the change is in the current displayed chat
+      if(parsedMessage.chatId==currentChatroomId){
+        loadChatroom(currentChatroomId);
+      }
+  }
 }
 
-socket.onmessage = (event)=>{
-    const recievedData = event.data;
-    console.log(recievedData);
-}
+////////////////////////////////////////////////////////////////////////////////////
+///USERNAME/////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
 
 //Username
 username.addEventListener("blur", ()=>{
     prefix=` ${username.value} > `;
     inputText.value = prefix;
-    console.log("heloo");
+    let previousUsername = clientChatrooms.get(currentChatroomId).chatroomUsername;
+    clientChatrooms.get(currentChatroomId).chatroomUsername = username.value;
+    sendChangedUsernameToServer(previousUsername);
 });
 
+function sendChangedUsernameToServer(previousUsername){
+  socket.send(JSON.stringify({
+    type:"username",
+    data:{
+      username: username.value,
+      previousUsername: previousUsername
+    },
+    chatId:currentChatroomId,
+  }))
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+///RENDER///////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+
+    const chatroomIdText_Client = document.querySelector(".chatroom-id");
+    const chatName_Client = document.querySelector(".chat-name");
+    const messages_Client = document.querySelector(".messages");
+    const currentMessage_Client = document.querySelector(".text-input");
+    const users_Client = document.querySelector("#connectedUsers");
+
+    //Currently loadChatroom is the Re render function, it rerenders all the elements of the chat like messages, users...
+      //its better to divide the isolated functionalities so, in the future, when only the users need to be updated, you dont need to reload the hole page
+    function loadChatroom(id){
+      currentChatroomId = id;
+      const chatroomToLoad = clientChatrooms.get(id);
+      username.value = chatroomToLoad.chatroomUsername;
+      loadUsers(id);
+      loadMessages(id); 
+      chatName_Client.textContent = chatroomToLoad.chatroomName;
+      chatroomIdText_Client.textContent = "CHATROOM ID: "+id;
+      currentMessage_Client.value = chatroomToLoad.currentMsg == "" ? (" "+username.value + " > ") : chatroomToLoad.currentMsg;
+    }
+    function loadMessages(id){
+        messages_Client.textContent = "";
+        clientChatrooms.get(id).messages.forEach(message => {
+          let textMessage = document.createElement('p');
+          textMessage.textContent=`${message}`;
+          textMessage.classList="message";
+          messages.prepend(textMessage);
+        });
+    }
+    function loadUsers(id){
+      users_Client.textContent = "";
+        clientChatrooms.get(id).usernames.forEach(user=>{
+          let textUser = document.createElement('p');
+          textUser.textContent=user;
+          users_Client.appendChild(textUser);
+        });
+    }
+    
+
+
+   
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+////////////////////////////////////////////////////////////////////////////////////
+/// SERVER CREATION ////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
 const http = require('http');
 const fs = require('fs/promises');
-const {join, extname} = require('path');
+const {join, extname, parse} = require('path');
 const WebSocket = require('ws');
+const { json } = require('stream/consumers');
 
 const PORT = 3001;  
 
@@ -38,22 +42,110 @@ webSocketServer.on("close",()=>{
     console.log("Good bye, server closing!");
 })
 
+const serverChatrooms = new Map();
+
 webSocketServer.on("connection",(socket)=>{
     //when the socket is Open, log a confirmation message on CLIENT side
     socket.on("open",()=>{
         console.log("a connection has been open");
     });
-    //Server have an event that when recieved a message from the client, send it back to all the clients except himself
-        //in future revisions and iterations, this cannot happen with ALL messages
+
+////////////////////////////////////////////////////////////////////////////////////
+/// SERVER MESSAGE HANDLER /////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+
     socket.addEventListener("message",(message)=>{
-        console.log('Mensaje recibido del cliente:', message.data);
-        webSocketServer.clients.forEach((client)=>{
-            if(client!=socket){
-                client.send(message.data);
+        const parsedMessage = JSON.parse(message.data);
+        switch(parsedMessage.type){
+
+            //TEXT MESSAGES
+            case "textMessage":
+                serverChatrooms.get(parsedMessage.chatId).clients.forEach((client)=>{
+                    if(client!=socket){
+                        client.send(JSON.stringify(parsedMessage));
+                    }
+                });
+                break;
+            //CREATION OF CHATROOM
+            case "chatroomCreationPetition":
+                let chatRoomId = createChatroomCode();
+                console.log("CHATROOM CREATED SUCCESFULLY WITH ID: "+chatRoomId)
+                socket.send(serverMessageFormat("chatroomCode",chatRoomId));
+                serverChatrooms.set(chatRoomId,{
+                    clients:[socket],
+                    chatroomData:{
+                        lastMessages:[],
+                        usernames:[],
+                        chatroomName:""
+                    }
+                }
+                );
+                break;
+            //JOINING A CHATROOM
+            case "chatroomJoinPetition":
+                let desiredChatroomId = parsedMessage.data.chatroomId;
+                let desiredChatroom = serverChatrooms.get(desiredChatroomId);
+                //Send the new User the authority to create the chatroom
+                socket.send(serverMessageFormat("chatroomCode",desiredChatroomId));
+                //For each other clients of that chat room, send them the new Username to update their sidebar
+                desiredChatroom.clients.forEach(client=>{
+                    client.send(JSON.stringify({
+                        type:"username",
+                        data:{
+                            username:parsedMessage.data.username,
+                            previousUsername:null
+                        },
+                        chatId: desiredChatroomId
+                    }));
+                })
+                //add the new user to the ServerChatroom
+                serverChatrooms.get(desiredChatroomId).clients.push(socket);
+                //send last 3 messages
+                desiredChatroom.chatroomData.lastMessages.forEach(message=>{
+                    socket.send(serverMessageFormat("textMessage",message,desiredChatroomId));
+                });
+                
+                //send chatroom name
+                socket.send(serverMessageFormat("chatroomName",desiredChatroom.chatroomData.chatroomName,desiredChatroomId));
+                break;
+                //USERNAME CHANGE
+                case "username":
+                    const currentRoom = serverChatrooms.get(parsedMessage.chatId);
+                    
+                    if (!currentRoom) break;
+
+                    const roomUsernames = currentRoom.chatroomData.usernames;
+                    let usernameChange = false;
+
+                    for (let i = 0; i < roomUsernames.length; i++) {
+                        if (roomUsernames[i] === parsedMessage.data.previousUsername) {
+                            roomUsernames[i] = parsedMessage.data.username;
+                            usernameChange = true;
+                            break;
+                        }
+                    }
+
+                    if (!usernameChange && !roomUsernames.includes(parsedMessage.data.username)) {
+                        roomUsernames.push(parsedMessage.data.username);
+                    }
+
+                    currentRoom.clients.forEach((client) => {
+                        if (client != socket) {
+                            client.send(JSON.stringify(parsedMessage));
+                        }
+                    });
+                break;
+
+            
+            default:
+                console.warn("Message type not recognized");
             }
         })
-    })
-})
+    });
+
+    function createChatroomCode(){
+        return Math.floor(Math.random() * 1000000000).toString();
+    }
 
 
 
@@ -62,3 +154,10 @@ server.listen(3001, () => {
   console.log('Servidor escuchando en http://localhost:3001');
 });
 
+function serverMessageFormat(type,data=null,chatId){
+    return JSON.stringify({
+        type: type,
+        data: data,
+        chatId:chatId
+    })
+}
